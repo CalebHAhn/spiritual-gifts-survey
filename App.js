c22b2cb64537a74f3aab4994e69b80709967955e
Caleb <ahn99@purdue.edu>
2023-08-23 10:51:24 -0700

    title plus basic coloring

--- App.js
+++ App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
-
-
 export default function App() {
   return (
     <View style={styles.container}>
       <ScrollView>
-      <Text style={title}>Spiritual Gifts Survey</Text>
-      <Text>Church</Text>
+      <Text style={styles.titleText}>Spiritual Gifts Survey</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
       </ScrollView>
     </View>
@@ -19,12 +18,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#b0d8ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  baseText: {
+    fontFamily: 'Cochin',
+  },
+  titleText: {
+    fontFamily: 'Monaco, Helvetica, Arial, Times New Roman',
+    fontSize: 64,
+    fontWeight: 'bold',
+    color: '#2058ff',
+  },
 });
-const title = { 
-    fontFamily: "Times New Roman",
-    fontSize: 36,
-};
